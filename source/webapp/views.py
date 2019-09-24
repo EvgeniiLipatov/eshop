9de16c346be161ea703e5939224c711598e16983
Evgenii <varlock7@gmail.com>
2019-09-24 16:25:00 +0600

    second stage

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.models import Product, CATEGORY_CHOICES
 
-# Create your views here.
+def index_view(request, *args, **kwargs):
+    products = Product.objects.filter(balance__gt=0).order_by('name').order_by('category')
+    return render(request, 'index.html', context={
+        'products': products
+    })
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', context={
+        'product': product
+    })
