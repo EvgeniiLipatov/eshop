21b7d9a8bc79d1c6aefece541843e8979e68624a
Evgenii <varlock7@gmail.com>
2019-09-24 07:12:34 +0600

    Add first migration

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'description', 'category', 'balance', 'price']
+    list_filter = ['category']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'category', 'price']
+    fields = ['name', 'description', 'category']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('телефоны', 'smartphones'),
+    ('весы', 'weights'),
+    ('сувениры', 'souvenirs'),
+    ('иное', 'others')
+)
+
+
+class Product(models.Model):
+    name = models.TextField(max_length=100, null=False, blank=False, verbose_name='наименование товара')
+    description = models.TextField(max_length=2000, blank=True, default='', verbose_name='описание')
+    category = models.CharField(max_length=40, null=False, blank=False, verbose_name="Статус",
+                                default=CATEGORY_CHOICES[0][0], choices=CATEGORY_CHOICES)
+    balance = models.PositiveIntegerField(blank=True, verbose_name = "остаток")
+
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, verbose_name='цена')
+
+    def __str__(self):
+        return self.description
